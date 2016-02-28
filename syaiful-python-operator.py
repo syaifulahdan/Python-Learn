4b01936694f147a4185476d584dcf0f3f7a36b27
Syaiful Ahdan <syaifulahdan@outlook.com>
2016-02-28 23:30:40 +0700

    Update syaiful-python-operator.py

--- syaiful-python-operator.py
+++ syaiful-python-operator.py
@@ -1,6 +1,4 @@
-# lat6.py
-# Operator dan ekspresi
-
+print '-' * 80
 bilangan1 = 5
 bilangan2 = 3
 print 'bil1 = ', bilangan1
@@ -9,6 +7,9 @@ print 'bil1 + bil2 = ', bilangan1 + bilangan2
 print 'bil1 - bil2 = %s' % (bilangan1 - bilangan2)
 print 'bil1 * bil2 = {0}'.format(bilangan1 * bilangan2)
 print 'bil1 ** bil2 = ', bilangan1 ** bilangan2
+print '-' * 80
+
+
 bilangan1 = 5.0
 print 'bil1 = ', bilangan1
 print 'bil2 = ', bilangan2
@@ -16,42 +17,30 @@ print 'bil1 / bil2 = ', bilangan1 / bilangan2
 print 'bil1 // bil2 = ', bilangan1 // bilangan2
 print 'bil1 % bil2 = ', bilangan1 % bilangan2
 print '-' * 80
+
+
 bilangan1 = 5
 print 'bil1 = ', bilangan1
 print 'bil2 = ', bilangan2
-print
-print
-print
-print
-print
-print
-'bil1 << bil2 = ', bilangan1 << bilangan2
-'bil1 >> bil2 = ', bilangan1 >> bilangan2
-'bil1 & bil2 = ', bilangan1 & bilangan2
-'bil1 | bil2 = ', bilangan1 | bilangan2
-'bil1 ^ bil2 = ', bilangan1 ^ bilangan2
-'~bil1 = ', ~bilangan1
-print '-' * 80
-print
-print
-print
-print
-print
-print
-'bil1
-'bil1
-'bil1
-'bil1
-'bil1
-'bil1
 
-< bil2 = ', bilangan1 < bilangan2
-> bil2 = ', bilangan1 > bilangan2
-<= bil2 = ', bilangan1 <= bilangan2
->= bil2 = ', bilangan1 >= bilangan2
-== bil2 = ', bilangan1 == bilangan2
-!= bil2 = ', bilangan1 != bilangan2
+print 'bil1 << bil2 = ', bilangan1 << bilangan2
+print'bil1 >> bil2 = ', bilangan1 >> bilangan2
+print'bil1 & bil2 = ', bilangan1 & bilangan2
+print'bil1 | bil2 = ', bilangan1 | bilangan2
+print'bil1 ^ bil2 = ', bilangan1 ^ bilangan2
+print '~bil1 = ', ~bilangan1
 print '-' * 80
+
+print 'bil1 < bil2 = ', bilangan1 < bilangan2
+print 'bil1 > bil2 = ', bilangan1 > bilangan2
+print 'bil1 <= bil2 = ', bilangan1 <= bilangan2
+print 'bil1 >= bil2 = ', bilangan1 >= bilangan2
+print 'bil1 == bil2 = ', bilangan1 == bilangan2
+print 'bil1 != bil2 = ', bilangan1 != bilangan2
+print '-' * 80
+
 print 'not True = ', not True
 print 'True and False = ', True and False
 print 'True or False = ', True or False
+print '-' * 80
+
