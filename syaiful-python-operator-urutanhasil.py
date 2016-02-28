3062cd2d02d99e1eca8b008d70a5a49bd40b7fbd
Syaiful Ahdan <syaifulahdan@outlook.com>
2016-02-29 00:19:38 +0700

    Create syaiful-python-operator-urutanhasil.py

---
+++ syaiful-python-operator-urutanhasil.py
@@ -0,0 +1,15 @@
+hasil = 2 + 3 * 4
+print '2 + 3 * 4 = %s' % hasil
+print '-' * 50
+
+hasil = (2 + 3) * 4
+print '(2 + 3) * 4 = %s' % hasil
+print '-' * 50
+
+hasil = 2 / 3 * 4
+print '2 / 3 * 4 = %s' % hasil
+print '-' * 50
+
+hasil = 2.0 / 3 * 4
+print '2.0 / 3 * 4 = %s' % hasil
+print '-' * 50
